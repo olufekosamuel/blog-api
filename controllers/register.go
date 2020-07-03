f04652749f9cc6b846c2a43d4fda24225ef47a9c
olufekosamuel <olufekosamuel@gmail.com>
2020-07-03 02:01:39 +0100

    version 1.3

--- controllers/register.go
+++ controllers/register.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
-
+	"encoding/json"
+	"fmt"
+	"github.com/olufekosamuel/blog-api/auth"
+	"github.com/olufekosamuel/blog-api/helpers"
+	"github.com/olufekosamuel/blog-api/models"
+	"github.com/lithammer/shortuuid"
+	"net/http"
+	"time"
 )
 
 func Register(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("content-type", "application/json")
 
 	if (*r).Method == "POST":
-		
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module blog-api
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.7.0
